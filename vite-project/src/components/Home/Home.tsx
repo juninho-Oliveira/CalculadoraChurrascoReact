e891da683aa5eb937b832a1264ed9c00c520a655
impressao recordalbum <junior.oliveira05@icloud.com>
2024-03-12 13:26:24 -0300

    add lista

--- vite-project/src/components/Home/Home.tsx
+++ vite-project/src/components/Home/Home.tsx
@@ -1,8 +1,7 @@
+
 import data from '../DbJson/db.json'
 
 const Home = () => {
-
-    console.log(data)
 
     return (
         <div>
@@ -11,9 +10,9 @@ const Home = () => {
                 <button className="bg-indigo-500 w-36 h-10 rounded-lg text-white text-lg font-semibold hover:bg-indigo-700" >Criar Churrasco</button>
             </header>
 
-            <main className=' flex  w-full  h-24 justify-center gap-5 items-center'>
+            <main className='flex  w-full justify-center gap-5 items-center'>
                 <table>
-                    <tr className=' flex gap-2'>
+                    <tr className='flex gap-20'>
                         <th>Data</th>
                         <th>Quantidade de Pessoas</th>
                         <th>Carne (Kg)</th>
@@ -24,7 +23,19 @@ const Home = () => {
                         <th>Ação</th>
                     </tr>
                     <tbody>
-                        
+                        {data.ListaChurrascos.map(item => (
+                            <td className='flex gap-2 border-2 border-black items-center rounded-lg justify-center'>
+                                {item.dataChurrasco}
+                                {item.quantidadePessoas}
+                                {item.quantidadeCarne}
+                                {item.quantidadePaoAlho}
+                                {item.quantidadeCarvao}
+                                {item.quantidadeRefri}
+                                {item.quantidadeCerveja}
+                                <button className="bg-indigo-500 w-12 h-8 rounded-lg text-white text-sm font-semibold hover:bg-indigo-700">Editar</button>
+                                <button className="bg-indigo-500 w-12 h-8 rounded-lg text-white text-sm font-semibold hover:bg-indigo-700">Excluir</button>
+                            </td>
+                        ))}
                     </tbody>
                 </table>
             </main>
