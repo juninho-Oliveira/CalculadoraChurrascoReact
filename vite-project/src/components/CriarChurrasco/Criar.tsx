f1d2fc7f622981f179d30e546f68f91cd8904abf
junior <junior.oliveira 05@icloud.com>
2024-03-30 12:20:11 -0300

    add render

--- vite-project/src/components/CriarChurrasco/Criar.tsx
+++ vite-project/src/components/CriarChurrasco/Criar.tsx
@@ -106,7 +106,7 @@ const Formulario = () => {
     setChurrascos(currentChurrascos => [...currentChurrascos, dados]);
 
     try {
-      const response = await axios.post('https://api.render.com/deploy/srv-cnu24gv79t8c73cc1h90?key=rUyr-wnHvQY', dados);
+      const response = await axios.post('https://jsonserve-aulaada.onrender.com/ListaChurrascos', dados);
       console.log('Churrasco adicionado:', response.data);
       navigate('/');
     } catch (error) {
